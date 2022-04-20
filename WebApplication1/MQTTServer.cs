ec69377271c699b13eb67508d0426fad3f700a10
黄锦灿 <huangjincan163@163.com>
2022-04-20 18:36:07 +0800

    添加子模块

--- WebApplication1/MQTTServer.cs
+++ WebApplication1/MQTTServer.cs
@@ -11,28 +11,50 @@ namespace WebApplication1
         }
 
         IMqttServer? _server;
+
+        /// <summary>
+        /// 启动服务
+        /// </summary>
         public async void StartServe()
         {
             //链式编程
-            var optionBuilder = new MqttServerOptionsBuilder()
-                            .WithDefaultEndpoint()
-                            .WithDefaultEndpointPort(1883)
-                            .WithConnectionValidator((c) =>
-                            {
-                                //连接验证
-                                c.ReasonCode = MQTTnet.Protocol.MqttConnectReasonCode.Success;
-                            })
-                            .WithSubscriptionInterceptor((c) =>
-                            {
-                                //允许订阅主题
-                                c.AcceptSubscription = true;
-                            })
-                            .WithApplicationMessageInterceptor((c) =>
-                            {
-                                //允许发布主题
-                                c.AcceptPublish = true;
-                            });
+            MqttServerOptionsBuilder optionBuilder = GetDefaultOptionBuilder();
             _server = new MqttFactory().CreateMqttServer();
+            UseHook();
+            await _server.StartAsync(optionBuilder.Build());
+        }
+
+        /// <summary>
+        /// 获取已经用默认设置设置好的 MqttServerOptionsBuilder
+        /// </summary>
+        /// <returns></returns>
+        MqttServerOptionsBuilder GetDefaultOptionBuilder()
+        {
+            return new MqttServerOptionsBuilder()
+                             .WithDefaultEndpoint()
+                             .WithDefaultEndpointPort(1883)
+                             .WithConnectionValidator((c) =>
+                             {
+                                 //连接验证
+                                 c.ReasonCode = MQTTnet.Protocol.MqttConnectReasonCode.Success;
+                             })
+                             .WithSubscriptionInterceptor((c) =>
+                             {
+                                 //允许订阅主题
+                                 c.AcceptSubscription = true;
+                             })
+                             .WithApplicationMessageInterceptor((c) =>
+                             {
+                                 //允许发布主题
+                                 c.AcceptPublish = true;
+                             });
+        }
+
+        /// <summary>
+        /// 使用钩子
+        /// </summary>
+        void UseHook()
+        {
             _server.UseClientConnectedHandler(c =>
             {
                 Console.WriteLine("客户端已连接");
@@ -41,8 +63,6 @@ namespace WebApplication1
             {
                 Console.WriteLine("客户端断开连接");
             });
-            await _server.StartAsync(optionBuilder.Build());
         }
-
     }
 }
--- WebApplication1/Program.cs
+++ WebApplication1/Program.cs
@@ -1,17 +1,22 @@
 using System.Text;
 using WebApplication1;
+using WebServerLib;
 
 new MQTTServer();
+string rootPath = @"D:/my_files/workspace/前端/CLI/vue_test/dist";
 
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 WebApplication app = builder.Build();
 
 RequestDelegate requestDelegate = async (HttpContext context) =>
   {
-      HttpResponse re = context.Response;
-      re.StatusCode = 200;
-      re.ContentType = "text/plain";
-      Stream body = re.Body;
+      HttpResponse response = context.Response;
+      HttpRequest request = context.Request;
+      Console.WriteLine(request.Path);
+      response.StatusCode = 200;
+      response.ContentType = Mime.GetContextType("a.txt");
+      Console.WriteLine(response.ContentType);
+      Stream body = response.Body;
       string hello = "hollo world";
       byte[] helloByte = Encoding.UTF8.GetBytes(hello);
       await body.WriteAsync(helloByte, 0, helloByte.Length);
