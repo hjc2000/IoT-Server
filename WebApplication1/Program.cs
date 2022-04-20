3a0a4571299f72183ad92295f23bb97a0e6c9ff6
黄锦灿 <huangjincan163@163.com>
2022-04-20 12:10:44 +0800

    1

--- WebApplication1/Program.cs
+++ WebApplication1/Program.cs
@@ -1,6 +1,18 @@
+using System.Text;
+
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 WebApplication app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+RequestDelegate re = async (HttpContext context) =>
+  {
+      HttpResponse re = context.Response;
+      re.StatusCode = 200;
+      re.ContentType = "text/plain";
+      Stream body = re.Body;
+      string hello = "hollo world";
+      byte[] helloByte = Encoding.UTF8.GetBytes(hello);
+      await body.WriteAsync(helloByte, 0, helloByte.Length);
+  };
+app.MapGet("/", re);
 
 app.Run();
