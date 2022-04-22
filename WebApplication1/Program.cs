5572251e1bcf95ce0199c81869b51d75f566b806
黄锦灿 <huangjincan163@163.com>
2022-04-22 11:56:28 +0800

    1

--- WebApplication1/Program.cs
+++ WebApplication1/Program.cs
@@ -3,24 +3,39 @@ using WebApplication1;
 using WebServerLib;
 
 new MQTTServer();
-string rootPath = @"D:/my_files/workspace/前端/CLI/vue_test/dist";
-
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 WebApplication app = builder.Build();
-
-RequestDelegate requestDelegate = async (HttpContext context) =>
-  {
-      HttpResponse response = context.Response;
-      HttpRequest request = context.Request;
-      Console.WriteLine(request.Path);
-      response.StatusCode = 200;
-      response.ContentType = Mime.GetContextType("a.txt");
-      Console.WriteLine(response.ContentType);
-      Stream body = response.Body;
-      string hello = "hollo world";
-      byte[] helloByte = Encoding.UTF8.GetBytes(hello);
-      await body.WriteAsync(helloByte, 0, helloByte.Length);
-  };
-app.MapGet("/", requestDelegate);
-
+app.UseWebSockets();
+app.UseRouting();//使用路由中间件
+app.Map("/ws", async (HttpContext context) =>
+{
+    if (context.WebSockets.IsWebSocketRequest)
+    {
+        Console.WriteLine("收到websocket请求");
+        //接收 WebSocket 对象后传递给 Echo 函数去维护 WebSocket 句柄
+        var webSocket = await context.WebSockets.AcceptWebSocketAsync("chat");
+        ArraySegment<byte> arrSegment = new ArraySegment<byte>(new byte[1024]);
+        CancellationTokenSource source = new CancellationTokenSource();
+        CancellationToken token = source.Token;
+        while (true)
+        {
+            await webSocket.ReceiveAsync(arrSegment, token);
+            byte[] dataArr = arrSegment.ToArray();
+            string str = Encoding.UTF8.GetString(dataArr);
+            try
+            {
+                str = str.Remove(str.LastIndexOf("。"));//找到句号，没有找到会引发异常
+                Console.WriteLine(str);
+            }
+            catch
+            {
+                Console.WriteLine("执行web发来的命令时发生异常");
+            }
+        }
+    }
+});
+app.UseFileServer();//启用文件服务
 app.Run();
+
+
+
